36cd91d7defead84dfa4fb1e316221f73cc41462
tyngw <env.dev.tk@gmail.com>
2026-01-11 18:08:27 +0900

    test: implement new E2E test framework with @vscode/test-electron and Playwright

--- test/runTest.ts
+++ test/runTest.ts
@@ -1,1 +1,28 @@
-module.exports = require('../src/test/runTest');
+// @vscode/test-electron メインエントリポイント
+
+import * as path from 'path';
+import { runTests } from '@vscode/test-electron';
+
+async function main() {
+    try {
+        // The folder containing the Extension Manifest package.json
+        const extensionDevelopmentPath = path.resolve(__dirname, '../');
+
+        // Minimal launch args for testing
+        const launchArgs = ['--disable-extensions'];
+
+        // Download VS Code, unzip it and run the integration test
+        // Note: VS Code test runtime will be cached in ~/.vscode-test/ by default
+        await runTests({
+            extensionDevelopmentPath,
+            extensionTestsPath: path.resolve(__dirname, '../test/vscode-tests'),
+            launchArgs,
+            version: 'stable',
+        });
+    } catch (err) {
+        console.error('Failed to run tests:', err);
+        process.exit(1);
+    }
+}
+
+main();
